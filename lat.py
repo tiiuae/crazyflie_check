7e6590b6e3acee1e9e3f9e4ed654264c05fb9abf
Humaid <humaid@ssrc.tii.ae>
2021-08-22 15:03:34 +0400

    Initial commit

---
+++ lat.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2021 Bitcraze AB
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA  02110-1301, USA.
+import struct
+import time
+import unittest
+
+import numpy as np
+
+import cflib.crtp
+from cflib.crtp.crtpstack import CRTPPacket
+from cflib.crtp.crtpstack import CRTPPort
+
+def latency(uri, packet_size=8, count=500):
+    link = cflib.crtp.get_link_driver(uri)
+    # # wait until no more packets in queue
+    # while True:
+    #     pk = link.receive_packet(0.5)
+    #     print(pk)
+    #     if not pk or pk.header == 0xFF:
+    #         break
+
+    pk = CRTPPacket()
+    pk.set_header(CRTPPort.LINKCTRL, 0)  # Echo channel
+
+    latencies = []
+    for i in range(count):
+        pk.data = build_data(i, packet_size)
+
+        start_time = time.time()
+        link.send_packet(pk)
+        while True:
+            pk_ack = link.receive_packet(-1)
+            if pk_ack.port == CRTPPort.LINKCTRL and pk_ack.channel == 0:
+                break
+        end_time = time.time()
+
+        # make sure we actually received the expected value
+        i_recv, = struct.unpack('<I', pk_ack.data[0:4])
+        assert(i == i_recv)
+        latencies.append((end_time - start_time) * 1000)
+    link.close()
+    result = np.average(latencies)
+    #print('Latency for {} (packet size {} B): {:.2f} ms'.format(uri, packet_size, result))
+    return result
+
+def build_data(i, packet_size):
+    assert(packet_size % 4 == 0)
+    repeats = packet_size // 4
+    return struct.pack('<' + 'I'*repeats, *[i]*repeats)
---
+++ main.py
@@ -0,0 +1,145 @@
+import logging
+import time
+import sys
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.utils import uri_helper
+
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncLogger import SyncLogger
+import colorama
+
+from lat import latency
+
+colorama.init(autoreset=True)
+def_uri = "radio://0/30/2M/E7E7E7E7"
+
+drones = {
+    #"cf00": "00",
+    #"cf01": "01",
+    "cf02": "02"
+    #"cf03": "03",
+    #"cf04": "04",
+    #"cf05": "05",
+    #"cf06": "06",
+    #"cf07": "07",
+    #"cf08": "08",
+    #"cf09": "09",
+    #"cf10": "10",
+    #"cf11": "11"
+}
+
+drone_data = {}
+
+
+logging.basicConfig(level=logging.ERROR)
+
+lat_packet_size=24
+lat_count=500
+
+print("Latency settings: packet_size=%d count=%d\n"  % (lat_packet_size, lat_count))
+
+def vari(a):
+    max_roll=-sys.maxsize-1
+    min_roll=sys.maxsize
+    max_pitch=-sys.maxsize-1
+    min_pitch=sys.maxsize
+    max_yaw=-sys.maxsize-1
+    min_yaw=sys.maxsize
+    for x in a:
+        #print("%d %d %d" % x)
+        roll = x[0]
+        pitch = x[1]
+        yaw = x[2]
+        if max_roll < roll:
+            max_roll = roll
+        elif min_roll > roll:
+            min_roll = roll
+
+        if max_pitch < pitch:
+            max_pitch = pitch
+        elif min_pitch > pitch:
+            min_pitch = pitch
+        
+        if max_yaw < yaw:
+            max_yaw = yaw
+        elif min_yaw > yaw:
+            min_yaw = yaw
+    return (max_roll - min_roll, max_pitch - min_pitch, max_yaw - min_yaw)
+
+
+def print_bool(a):
+    if a:
+        print(colorama.Back.GREEN+colorama.Fore.BLACK+"OK", end='')
+    else:
+        print(colorama.Back.RED+"FAIL", end='')
+
+def simple_log(drone, scf, logconf):
+    print( drone+": "+colorama.Fore.GREEN + "Connected. Press enter and move device", end='')
+    input()
+    time.sleep(0.8)
+    with SyncLogger(scf, lg_stab) as logger:
+        i = 0
+        bat = 0.0
+        bat_stat = 0
+        entries = [ ]
+        for log_entry in logger:
+            #timestamp = log_entry[0]
+            data = log_entry[1]
+            #logconf_name = log_entry[2]
+            drone_data[drone] = data
+            bat = data['pm.vbat']
+            bat_stat = data['pm.state']
+
+            #print('%s: [%d][%s]: %s' % (drone, timestamp, logconf_name, data))
+            entries.append((data['stabilizer.roll'], data['stabilizer.pitch'], data['stabilizer.yaw']))
+            i=i+1
+            if i > 90:
+                break
+        print(drone +':'+colorama.Fore.CYAN +' OK (Bat: %s volts)' % (bat))
+        if bat_stat == BatteryStates.LOW_POWER:
+            print(colorama.Fore.RED + "Low Battery")
+        varis = vari(entries)
+        roll, pitch, yaw = (varis[0] > 20), (varis[1] > 20), (varis[2] > 5)
+        print("Roll: ", end='')
+        print_bool(roll)
+        print(", Pitch: ", end='')
+        print_bool(pitch)
+        print(", Yaw: ", end='')
+        print_bool(yaw)
+        print("")
+
+
+
+class BatteryStates:
+    BATTERY, CHARGING, CHARGED, LOW_POWER = list(range(4))
+
+if __name__ == '__main__':
+    # Initialize the low-level drivers
+    cflib.crtp.init_drivers()
+
+    for x in drones:
+        lg_stab = LogConfig(name='Stabilizer', period_in_ms=10)
+        lg_stab.add_variable('stabilizer.roll', 'float')
+        lg_stab.add_variable('stabilizer.pitch', 'float')
+        lg_stab.add_variable('stabilizer.yaw', 'float')
+        lg_stab.add_variable('pm.vbat', 'float')
+        lg_stab.add_variable('pm.state', 'int8_t')
+        uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7'+drones[x])
+
+        try:
+            with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+                simple_log(x, scf, lg_stab)
+            lat_ms = latency(uri, lat_packet_size, lat_count)
+            col=colorama.Fore.GREEN
+            if lat_ms > 10:
+                col=colorama.Fore.RED
+            elif lat_ms > 8:
+                col=colorama.Fore.YELLOW
+            print("Latency: "+col+"%dms" % lat_ms)
+            print("")
+        except Exception as e:
+            print(colorama.Fore.RED + x+" failed: ", e)
+
